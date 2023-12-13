56449b1b9fae73bef40406cded4b205b188cf42f
ashleymuka <153102801+ashleymuka@users.noreply.github.com>
2023-12-12 22:50:10 -0600

    Update randomGuess.py

--- randomGuess.py
+++ randomGuess.py
@@ -1,22 +1,15 @@
 '''
-Author: Ashley Muka
-Assignment Title: Random Guess
-Assignment Description:implemet the number_guess() function to output
+Description:implemet the number_guess() function to output
 if the number guessed is too low, too high, or correct
-Due Date:09/18/2023
-Date Created:09/18/2023
-Date Last Modified:09/18/2023
 
 '''
 
 import random
 
-#data abstraction
 def number_guess(num):
     
     rand_num = random.randint(1, 100)
     
-#output
     if num < rand_num:
         print(f'{num} is too low. Random number was {rand_num}.')
     elif num > rand_num:
@@ -25,7 +18,7 @@ def number_guess(num):
         print(f'{num} is correct!')
 
 if __name__ == "__main__":
-#input and process   
+
     random.seed(900)
 
     user_input = input()
